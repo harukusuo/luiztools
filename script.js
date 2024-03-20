1d68f546551586a3e4b20626134079189a9d2749
sah <128996530+harukusuo@users.noreply.github.com>
2024-03-20 08:27:41 -0300

    upload de arquivos

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.querySelector('.navbar-toggler').addEventListener('click', function() {
+        document.querySelector('.navbar-collapse').classList.toggle('show');
+    });
+});
